7afd33f22ee4b5146bdd80be8543cab5cae679e8
TarCode <TarCode@github.com>
2015-06-25 13:43:40 +0200

    port fix

--- app.js
+++ app.js
@@ -154,12 +154,12 @@ app.get('/catProfit', nelisaSpaza.showCatProfit);
 
 
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('Nelisa app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port)
 
 })
-
