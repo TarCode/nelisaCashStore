a9a5ccc70b45582c033fda25af953d7f181f5db3
TarCode <taariq@projectcodex.co>
2015-07-22 10:18:36 +0200

    refactored category delete, stats and search data methods for testing

--- routes/categoryDataService.js
+++ routes/categoryDataService.js
@@ -21,7 +21,23 @@ module.exports = function (connection) {
   };
 
   this.getUpdateCategory = function (data, cb) {
-      insertData('SELECT * from category WHERE cat_id = ?', data, cb );
+      insertData('SELECT * FROM category WHERE cat_id = ?', data, cb );
+  };
+
+  this.deleteCategory = function (data, cb) {
+      insertData('DELETE FROM category WHERE cat_id = ?', data, cb );
+  };
+
+  this.searchCategory = function (data, cb) {
+      insertData('SELECT cat_id, cat_name from category WHERE cat_name LIKE ?', data, cb );
+  };
+
+  this.popularCategory = function (cb) {
+      getData('SELECT cat_name, sum(qtySold) as total_sold FROM product,category,sales  WHERE product.prod_id = sales.prod_id AND category.cat_id = product.cat_id GROUP BY cat_name ORDER BY total_sold DESC', cb );
+  };
+
+  this.profitsPerCategory = function (cb) {
+      getData('SELECT cat_name, supplier_name, sum(salePrice - cost) as profit FROM product,sales,stock,supplier, category where product.prod_id = sales.prod_id and product.prod_id = stock.prod_id and stock.supplier_id = supplier.supplier_id and product.cat_id = category.cat_id group by cat_name order by profit desc', cb );
   };
 
 };
--- routes/categoryMethods.js
+++ routes/categoryMethods.js
@@ -73,63 +73,43 @@ exports.updateCat = function (req, res, next) {
 };
 exports.delCat = function (req, res, next) {
     var cat_id = req.params.cat_id;
-
-    req.getConnection(function(err, connection){
-        if (err) return next(err);
-
-        connection.query('delete from category where cat_id = ?',[cat_id], function(err, results) {
-            if (err)
-                console.log("Error deleting : %s ",err );
-            res.redirect('/category');
-        });
+    catDataServ.deleteCategory([cat_id], function(err, results) {
+          if (err) return next(err);
+          res.redirect('/category');
     });
 };
 
 exports.showCatPopularity = function (req, res, next) {
-    req.getConnection(function(err, connection){
-        if (err)
-            return next(err);
-        connection.query('SELECT cat_name, sum(qtySold) as total_sold FROM product,category,sales  WHERE product.prod_id = sales.prod_id AND category.cat_id = product.cat_id GROUP BY cat_name ORDER BY total_sold DESC', [], function(err, results) {
-            if (err) return next(err);
-
-            res.render( 'catPopularity', {
-                catPopularity : results,
-                user: req.session.user,
-                admin:admin
-            });
+    catDataServ.popularCategory(function(err, results) {
+        if (err) return next(err);
+        res.render( 'catPopularity', {
+            catPopularity : results,
+            user: req.session.user,
+            admin:admin
         });
     });
 };
 exports.showCatProfit = function (req, res, next) {
-    req.getConnection(function(err, connection){
+    catDataServ.profitsPerCategory(function(err, results) {
         if (err) return next(err);
-        connection.query('select cat_name, supplier_name, sum(salePrice - cost) as profit from product,sales,stock,supplier, category where product.prod_id = sales.prod_id and product.prod_id = stock.prod_id and stock.supplier_id = supplier.supplier_id and product.cat_id = category.cat_id group by cat_name order by profit desc', [], function(err, results) {
-            if (err) return next(err);
-
-            res.render( 'catProfit', {
-                catProfit : results,
-                user: req.session.user,
-                admin:admin
-            });
+        res.render( 'catProfit', {
+            catProfit : results,
+            user: req.session.user,
+            admin:admin
         });
     });
 };
 
 exports.getSearchCategory = function(req, res, next){
-    req.getConnection(function(err, connection){
-        if(err)
-                return next(err);
-        var searchValue = req.params.searchValue;
-        searchValue = "%" + searchValue + "%";
-
-        connection.query("SELECT cat_id, cat_name from category WHERE cat_name LIKE ?",[searchValue], function(err, results){
-            if (err) return next(err);
-            res.render('category_list', {
-                admin: admin,
-                user: req.session.user,
-                categories : results,
-                layout : false
-            });
+    var searchValue = req.params.searchValue;
+    searchValue = "%" + searchValue + "%";
+    catDataServ.searchCategory([searchValue], function(err, results){
+        if (err) return next(err);
+        res.render('category_list', {
+            admin: admin,
+            user: req.session.user,
+            categories : results,
+            layout : false
         });
     });
 };
