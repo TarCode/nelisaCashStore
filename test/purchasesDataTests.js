1f20faeafd4722c73128481ca5e23459fd952fe4
Pholisa-Fatyela <pholisa@projectcodex.co>
2015-08-05 11:21:44 +0200

    purchasess inserting test

--- test/purchasesDataTests.js
+++ test/purchasesDataTests.js
@@ -15,3 +15,34 @@ describe('purchasesMethods: Display', function() {
     });
   });
 });
+
+describe('purchasesMethods: Alter', function() {
+
+  var data = {
+      purchase_id : 154
+  };
+
+  before(function(done) {
+    purchaseDataService.getAllPurchases(function(err, results){
+      assert.equal(153, results.length);
+      done();
+    });
+  });
+
+  it('insertPurchase: Should insert a purchase', function(done) {
+
+    purchaseDataService.insertPurchase(data, function(err, results){
+      purchaseDataService.getAllPurchases(function(err, results){
+        assert.equal(153, results.length);
+        done();
+      });
+
+    });
+
+    after(function(done){
+      connection.query("delete from stock where purchase_id = ?", data.purchase_id, function(err){
+        done(err);
+      });
+    })
+  });
+});
