989805768db39b7634afc7dc25d47b76e1341f6c
Pholisa-Fatyela <pholisa@projectcodex.co>
2015-08-05 12:12:50 +0200

    inserting a user test

--- test/usersDataTests.js
+++ test/usersDataTests.js
@@ -6,12 +6,40 @@ var connection = new Connection;
 connection.connect();
 var userDataService = new UserDataService(connection);
 
-describe('userMethods : Display', function(){
+describe('userMethods : Display', function() {
   it('getUsername: returns the username of the current user', function(done) {
     userDataService.getUsername(function(err, currentUsername) {
       username = currentUsername[0];
       assert.equal(username.username, 'lisa');
       done();
+    });
+  });
+});
+
+describe('userMethods : Alter', function() {
+  var testData = {
+    username : 'fakeUser'
+  };
+
+  before(function(done) {
+    userDataService.getUsername(function(err, numberOfUsers) {
+      assert.equal(2, numberOfUsers.length );
+      done();
+    });
+  });
+
+  it('insertUser: Should insert a user', function(done) {
+    userDataService.insertUser(function(err, newUser) {
+      userDataService.getUsername(function(err, users) {
+        assert.equal(2, users.length);
+        done();
+      });
+    });
+
+    after(function(done) {
+      connection.query("delete from users where username = ?", testData.username, function(err) {
+        done(err);
+      });
     });
   });
 });
